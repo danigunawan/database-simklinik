931243d9efd4dac269840f83bf0a0c3c41968551
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2016-12-07 10:53:14 +0700

    Ubah database

--- database/migrations/2016_11_18_043343_create_posts_table.php
+++ database/migrations/2016_11_18_043343_create_posts_table.php
@@ -17,6 +17,7 @@ class CreatePostsTable extends Migration
             $table->increments('id');
             $table->string('title')->unique();
             $table->string('content');
+            $table->string('contents');
             $table->timestamps();
         });
     }
